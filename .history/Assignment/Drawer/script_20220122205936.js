16a1e4237e041316bba730389ec5f14bebb141b8
anuj-kumary <anujf0510@gmail.com>
2022-01-23 00:01:45 +0530

    initial commit

--- .history/Assignment/Drawer/script_20220122205936.js
+++
@@ -1,9 +0,0 @@
-
- const header = document.querySelector(".drawer");
- const btns = header.querySelectorAll(".drawer__link");
- for (var i = 0; i < btns.length; i++) {
-   btns[i].addEventListener("click", function () {
-     var current = document.getElementsByClassName("active");
-     current[0].className = current[0].className.replace(" active", "");
-     this.className += " active";
-   });
